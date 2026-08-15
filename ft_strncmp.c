abc91d0705b2872c7c5f2c43141ce07127fc852e
JassCoder <sainijaspreet1999@gmail.com>
2026-08-15 07:29:42 +0200

    merged

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -5,21 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jsingh <jsingh@student.42warsaw.pl>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/13 12:21:50 by jsingh            #+#    #+#             */
-/*   Updated: 2026/08/13 12:21:50 by jsingh           ###   ########.fr       */
+/*   Created: 2026/08/12 19:17:59 by jsingh            #+#    #+#             */
+/*   Updated: 2026/08/12 19:17:59 by jsingh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	strncmp(const char *s1, const char *s2, size_t len)
+int ft_strcmp(char *s1, char *s2, size_t len)
 {
-	size_t	i;
+    int i;
 
-	i = 0;
-	while (i < len && s1[i] && s2[i] && s1[i] == s2[i])
-		i++;
-	if (i == len)
-		return (0);
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+    i = 0;
+    while (i < len && s1[i] && s2[i] && s1[i] == s2[i])
+        i++;
+    if (i == len)
+        return (0);
+    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
