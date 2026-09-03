8db318b3868d84508139719a9a40cc5ee0157f42
Malgorzata Szkudlarek <mszkudla@student.42warsaw.pl>
2026-09-03 14:53:54 +0200

    update adaptive strategy based on stack size

--- sorting/adaptive.c
+++ sorting/adaptive.c
@@ -17,10 +17,16 @@ void	adaptive_sort(t_stack *a, t_stack *b, t_bench *bench)
 	double	disorder;
 
 	disorder = calculate_disorder(a);
-	if (disorder < 0.20)
+	if (disorder < 0.1)
 		simple_sort(a, b, bench);
-	else if (disorder < 0.50)
-		medium_sort(a, b, bench);
+	else if(a->size < 430)
+		medium_sort(a,b,bench);
 	else
 		complex_sort(a, b, bench);
+	// if (disorder < 0.20)
+	// 	simple_sort(a, b, bench);
+	// else if (disorder < 0.50 )
+	// 	medium_sort(a, b, bench);
+	// else
+	// 	complex_sort(a, b, bench);
 }
