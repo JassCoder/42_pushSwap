53902dda777e9294000fde4cd57fcedf685e4e41
Malgorzata Szkudlarek <mszkudla@student.42warsaw.pl>
2026-09-04 13:02:53 +0200

    added moves count to bench report

--- benchmark/report.c
+++ benchmark/report.c
@@ -55,13 +55,39 @@ static void	putnbr_fd(int n, int fd)
 	write(fd, &c, 1);
 }
 
+void print_moves_count(t_config *config)
+{
+	putstr_fd("pa: ", 2);
+	putnbr_fd(config->count.pa, 2);
+	putstr_fd(" pb: ", 2);
+	putnbr_fd(config->count.pb, 2);
+	putstr_fd("\nsa: ", 2);
+	putnbr_fd(config->count.sa, 2);
+	putstr_fd(" sb: ", 2);
+	putnbr_fd(config->count.sb, 2);
+	putstr_fd(" ss: ", 2);
+	putnbr_fd(config->count.ss, 2);
+	putstr_fd("\nra: ", 2);
+	putnbr_fd(config->count.ra, 2);
+	putstr_fd(" rb: ", 2);
+	putnbr_fd(config->count.rb, 2);
+	putstr_fd(" rr: ", 2);
+	putnbr_fd(config->count.rr, 2);
+	putstr_fd("\nrra: ", 2);
+	putnbr_fd(config->count.rra, 2);
+	putstr_fd(" rrb: ", 2);
+	putnbr_fd(config->count.rrb, 2);
+	putstr_fd(" rrr: ", 2);
+	putnbr_fd(config->count.rrr, 2);
+}
+
 void	print_benchmark(t_config *config, double disorder)
 {
 	(void)disorder;
 	putstr_fd("Disorder: ",2);
 	putnbr_fd((disorder*1000)/10, 2);
 	putstr_fd(".", 2);
-	putnbr_fd((int)(disorder*1000)%100,2);
+	putnbr_fd((int)(disorder*1000)%100, 2);
 	putstr_fd("%\n",2);
 	putstr_fd("Strategy: ", 2);
 	putstr_fd(strategy_name(config->strategy), 2);
@@ -70,4 +96,5 @@ void	print_benchmark(t_config *config, double disorder)
 	putstr_fd("\nTotal operations: ", 2);
 	putnbr_fd(config->count.total, 2);
 	putstr_fd("\n", 2);
+	print_moves_count(config);
 }
